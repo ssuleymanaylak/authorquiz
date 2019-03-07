cf13c99092ee9eb75f3a2207ef03a12b0ebe6792
ssuleymanaylak <ssuleymanaylak@hotmail.com>
2019-03-07 13:04:35 +0300

    Added router

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
-import logo from './logo.svg';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './App.css';
 import './bootstrap.min.css';
 
-function Hero(){
+function Hero() {
   return (<div className="row">
     <div className="jumbotron col-10 offset-1">
       <h1>Author Quiz</h1>
@@ -20,7 +20,7 @@ function Book({title, onClick}) {
   );
 }
 
-function Turn({author,books,highlight,onAnswerSelected}) {
+function Turn({author, books, highlight, onAnswerSelected}) {
   function highlightToBgColor(highlight) {
     const mapping = {
       'none': '',
@@ -35,7 +35,7 @@ function Turn({author,books,highlight,onAnswerSelected}) {
       <img src={author.imageUrl} className="authorimage" alt="Author"/>
     </div>
     <div className="col-6">
-      {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected}/>)}
+      {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />)}
     </div>
   </div>);
 }
@@ -51,28 +51,38 @@ Turn.propTypes = {
   highlight: PropTypes.string.isRequired
 };
 
-function Continue() {
+function Continue({ show, onContinue }) {
+  return (
+    <div className="row continue">
+    { show 
+      ? <div className="col-11">
+          <button className="btn btn-primary btn-lg float-right" onClick={onContinue}>Continue</button>
+        </div>
+      : null }
+    </div>
+  );
+}
+
+function Footer() {
   return (<div id="footer" className="row">
     <div className="col-12">
-      <p className="text-muted credit">All images are from <a href="http://commons.https://commons.wikimedia.org/wiki/Main_Page.org/wiki/">Wikimedia Commons</a> and are in the public domain</p>
+        <p className="text-muted credit">
+          All images are from <a href="http://commons.wikimedia.org/wiki/Main_Page">Wikemedia Commons</a> and are in the public domain
+        </p>
     </div>
   </div>);
 }
 
-function Footer(){
-  return (<div/>);
-}
-
-
-function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
-    return (
-      <div className="container-fluid">
-          <Hero/>
-          <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
-          <Continue/>
-          <Footer/>
-      </div>
-    );
+function AuthorQuiz({turnData, highlight, onAnswerSelected, onContinue}) {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+      <Continue show={highlight === 'correct'} onContinue={onContinue}/>
+      <p><Link to="/add">Add an author</Link></p>
+      <Footer />
+    </div>
+  );
 }
 
 export default AuthorQuiz;
